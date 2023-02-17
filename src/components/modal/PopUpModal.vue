1e64517e18ad7774b68a0ae7e435e88b81c7838e
0andme <yu00mi97@gmail.com>
2023-02-17 16:44:06 +0900

    #3 feat : 팝업 모달 추가

---
+++ src/components/modal/PopUpModal.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- data-bs-toggle="modal"
+    data-bs-target="#exampleModal" -->
+  <button data-bs-toggle="modal" data-bs-target="#popUpModal">test</button>
+  <div
+    class="modal fade"
+    id="popUpModal"
+    tabindex="-1"
+    aria-labelledby="popUpModalLabel"
+    aria-hidden="true"
+  >
+    <!-- data-bs-dismiss="modal"
+            aria-label="Close" -->
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-body">
+          <div class="text">해당 행성을 생성하시겠습니까?</div>
+          <div class="btnList">
+            <ConfrimBtn
+              :btn-msg="'취소'"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></ConfrimBtn>
+            <ConfrimBtn :btn-msg="'확인'"></ConfrimBtn>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ConfrimBtn from "@/components/button/ConfrimBtn.vue";
+export default {
+  methods: {},
+  components: {ConfrimBtn},
+};
+</script>
+
+<style scoped>
+.modal-dialog {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.modal-content {
+  width: fit-content;
+  text-align: center;
+
+  border-radius: 20px;
+  background-color: var(--backGround);
+  border: 2px solid var(--modal-yellow);
+  box-sizing: content-box;
+}
+
+.modal-body {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  margin: 40px 26px;
+}
+.modal-body .text {
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 22px;
+  color: var(--text-white);
+  margin-bottom: 15px;
+}
+
+.modal-body .btnList {
+  display: flex;
+  gap: 20px;
+}
+</style>
