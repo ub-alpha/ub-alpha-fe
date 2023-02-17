a5b28d528645cca213d58d8eac211205a41ca4a5
0andme <yu00mi97@gmail.com>
2023-02-17 11:15:28 +0900

    #3 feat : 다음 버튼 컴포넌트 추가

---
+++ src/components/button/NextBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <button>다음</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+button {
+  color: var(--text-white);
+  width: 59px;
+  height: 36px;
+  border: 3px solid var(--text-white);
+  font-size: 17px;
+  text-align: center;
+  border-radius: 10px;
+}
+</style>
