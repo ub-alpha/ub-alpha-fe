29089712f2c191fd663babce498c406548c6b45e
0andme <yu00mi97@gmail.com>
2023-02-17 11:15:20 +0900

    #3 feat : 교환하기버튼 컴포넌트 추가

---
+++ src/components/button/ExChangeBtn.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>교환하기</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+button {
+  color: var(--text-white);
+  background-color: var(--mainColor);
+  padding: 5px 41px;
+  text-align: center;
+  border-radius: 10px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 17px;
+}
+</style>
