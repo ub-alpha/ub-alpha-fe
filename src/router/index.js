d06c8def09d1a84d2a92442d094c29757199768a
0andme <yu00mi97@gmail.com>
2023-02-17 09:16:28 +0900

    #1 conf : vue 프로젝트 시작

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: '',
+  //   component:
+  // },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
