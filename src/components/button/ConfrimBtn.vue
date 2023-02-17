da296d8ac609e57456631d0b2aeb3735573ed0e8
0andme <yu00mi97@gmail.com>
2023-02-17 11:15:13 +0900

    #3 feat : 확인 버튼 컴포넌트 추가

---
+++ src/components/button/ConfrimBtn.vue
@@ -0,0 +1,36 @@
+<template>
+  <button :class="`${btnSet[btnMsg]}`">{{ btnMsg }}</button>
+</template>
+
+<script>
+export default {
+  props: ["btnMsg"],
+  data() {
+    return {
+      btnSet: {
+        확인: "confirm",
+        취소: "cancel",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+button {
+  padding: 4px 18.5px;
+  font-size: 12px;
+  font-weight: 400;
+  border-radius: 10px;
+  box-sizing: content-box;
+  line-height: 17px;
+}
+.confirm {
+  background: var(--point);
+  color: var(--text-white);
+}
+.cancle {
+  background: var(--modal-yellow);
+  color: var(--text-black);
+}
+</style>
