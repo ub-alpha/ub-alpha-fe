adbc8c63e7f146e0030154bf7f121f506e7f5073
0andme <yu00mi97@gmail.com>
2023-02-18 18:45:28 +0900

    #3 feat : 상단 디바이스 상태 바 컴포넌트 추가

---
+++ src/components/DeviceStatusBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <img :src="require('@/assets/status-bar.png')" alt="상태바" />
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: fit-content;
+}
+header img {
+  width: 100%;
+}
+</style>
