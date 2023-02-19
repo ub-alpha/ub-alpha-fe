00d25143e4d7de9eba0e4a1634b8104b4ec10889
0andme <yu00mi97@gmail.com>
2023-02-19 11:56:39 +0900

    #2 feat : 메인 게임 화면 진입시 도움말 모달 띄우기

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,7 +1,24 @@
-<template></template>
+<template>
+  <div id="game">
+    <LongModal :modalName="'help'"></LongModal>
+  </div>
+</template>
 
 <script>
-export default {};
+import LongModal from "@/components/modal/LongModal.vue";
+export default {
+  components: { LongModal },
+
+  mounted() {
+    $("#longModal-help").modal("show");
+  },
+};
 </script>
 
-<style></style>
+<style>
+#game {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
