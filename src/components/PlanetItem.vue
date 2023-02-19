7f1be43d9b23cc9e196d89b0c8a294b84d9bd298
0andme <yu00mi97@gmail.com>
2023-02-20 00:26:51 +0900

    #2 feat : 행성 아이템 컴포넌트 추가

---
+++ src/components/PlanetItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="planet active carousel-item"></div>
+</template>
+
+<script>
+export default {
+  props: ["planet"],
+};
+</script>
+
+<style>
+.carousel-item {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+</style>
