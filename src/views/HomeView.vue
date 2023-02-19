5b955e58d56702a04c6878cd1a07f9bbf18118e5
0andme <yu00mi97@gmail.com>
2023-02-19 10:55:09 +0900

    #4 feat : 홈 화면 뷰 추가
    - 행성 게임으로 이동하는 버튼 추가
    - 배경 이미지 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,49 @@
-<template></template>
+<template>
+  <div id="home">
+    <router-link to="/start" class="space-btn">
+      <img src="@/assets/planet_icon.png" alt="행성 아이콘" />
+      <strong>나만의 행성</strong>
+    </router-link>
+  </div>
+</template>
 
-<script>
-export default {
-  components: {},
-};
-</script>
+<script></script>
+
+<style scoped>
+#home {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background: no-repeat top url("../assets/알파홈.png");
+  background-size: cover;
+}
+
+.space-btn {
+  width: calc(84 * 100% / 320);
+  position: absolute;
+  right: 9px;
+  top: calc(39% + 0.2rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 7px;
+  text-decoration: none;
+
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 1.4rem;
+
+  padding-top: calc(14 / 320 * 100%);
+  padding-bottom: calc(14 / 320 * 100%);
+  padding-right: calc(9 / 320 * 100%);
+  padding-left: calc(9 / 320 * 100%);
+
+  color: var(--text-white);
+  background-color: var(--backGround);
+
+  border-radius: 1rem;
+}
+.space-btn img {
+  width: 1rem;
+}
+</style>
