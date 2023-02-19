d388bef506751a12d32ccc0c5fb07fe3ec894d5d
0andme <yu00mi97@gmail.com>
2023-02-19 13:39:40 +0900

    #6 feat : 행성 이름- 포인트 데이터 store에 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,29 @@
-import {createStore} from "vuex";
+import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    member: {
+      member_id: 0,
+      spacename: "",
+      point: 0,
+    },
+    planets: [],
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    SET_SPACENAME(state, spacename) {
+      this.state.member.spacename = spacename;
+    },
+    SET_POINT(state, pointNum) {},
+  },
+  actions: {
+    GET_MEMBER(context) {},
+    GET_PLANETS(context) {},
+    // 행성 이름 설정
+    MAKE_SPACE(context, spacename) {
+      context.commit("SET_SPACENAME", spacename);
+    },
+    ADD_PLANET(context) {},
+  },
   modules: {},
 });
