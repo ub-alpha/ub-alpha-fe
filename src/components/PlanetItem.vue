d6fd1b0a3a9ae28e9e213d1b777472ba7bb38617
0andme <yu00mi97@gmail.com>
2023-02-20 09:53:35 +0900

    #2 feat : 행성 컴포넌트 추가

--- src/components/PlanetItem.vue
+++ src/components/PlanetItem.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="planet active carousel-item"></div>
+  <div :class="`planet ${index === 0 ? 'active' : ''} carousel-item`">
+    <div class="circle-wrap">
+      <img :src="require('@/assets/space-type-1.png')" alt="행성 생성하기" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["planet"],
+  props: ["planet", "index"],
 };
 </script>
 
 <style>
 .carousel-item {
   width: 100%;
+  height: 100%;
   position: absolute;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
+}
+
+.circle-wrap img {
+  display: block;
+  margin: 0 auto;
+  width: calc(250 / 320 * 100%);
 }
 </style>
