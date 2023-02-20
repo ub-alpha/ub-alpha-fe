a75d419c2c4c64b154a40bad839f473689dcc33e
0andme <yu00mi97@gmail.com>
2023-02-20 16:08:46 +0900

    #3 feat : 슬라이드 업 모달 스타일 수정

--- src/components/modal/SlideUpModal.vue
+++ src/components/modal/SlideUpModal.vue
@@ -1,20 +1,15 @@
 <template>
-  <!-- data-bs-toggle="modal"
-      data-bs-target="#exampleModal" -->
-  <button data-bs-toggle="modal" data-bs-target="#slideUpModal">222</button>
   <div
-    class="modal fade"
+    class="modal fade scroll"
     id="slideUpModal"
     tabindex="-1"
     aria-labelledby="slideUpModalLabel"
     aria-hidden="true"
   >
-    <!-- data-bs-dismiss="modal"
-              aria-label="Close" -->
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="header">
-          <h1 class="title" id="longModalLabel">할인권 교환</h1>
+          <h1 class="title" id="slideUpModalLabel">할인권 교환</h1>
           <button
             class="close-btn"
             type="button"
@@ -24,7 +19,9 @@
             <img :src="require('@/assets/close.svg')" alt="할인권 닫기" />
           </button>
         </div>
-        <div class="modal-body"></div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
       </div>
     </div>
   </div>
@@ -35,21 +32,34 @@ export default {};
 </script>
 
 <style scoped>
+.modal {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
 .modal-dialog {
   width: 100%;
-  position: relative;
-  animation: animatebottom 0.4s;
+  position: absolute;
+  bottom: 0;
+  animation: animatebottom linear 0.4s;
   margin: 0 auto;
 }
+@media (min-width: 576px) {
+  .modal-dialog {
+    max-width: 100%;
+  }
+}
 .modal-content {
-  width: 320px;
-  min-height: 259px;
+  width: 100%;
   text-align: center;
   margin: 0;
-  padding: 18px 29px;
-  border-radius: 20px 20px 0 0;
+  padding: 1.8rem 2.9rem;
+  border-radius: 2rem 2rem 0 0;
   background-color: var(--backGround);
-  border: 2px solid var(--modal-yellow);
+  border: 0.2rem solid var(--modal-yellow);
   box-sizing: border-box;
   position: relative;
 }
@@ -59,21 +69,24 @@ export default {};
 
 .modal-content .title {
   font-weight: 700;
-  font-size: 20px;
-  line-height: 29px;
+  font-size: 2rem;
+  line-height: 2.9rem;
   color: var(--text-white);
-  margin-top: 24px;
-  padding-bottom: 11px;
-  border-bottom: 2px solid var(--text-white);
+  margin-top: 2.4rem;
+  padding-bottom: 1.1rem;
+  border-bottom: 0.2rem solid var(--text-white);
 }
 .modal-content .close-btn {
   position: absolute;
-  right: 14px;
-  top: 14px;
+  right: 1.4rem;
+  top: 1.4rem;
+}
+.modal-content .close-btn img {
+  width: 2.4rem;
 }
 @keyframes animatebottom {
   from {
-    bottom: -300px;
+    bottom: -1000px;
     opacity: 0;
   }
 
