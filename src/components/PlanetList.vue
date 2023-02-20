c4952b5ad609266fceea1fce0b8834b0964ac350
0andme <yu00mi97@gmail.com>
2023-02-20 10:01:19 +0900

    #2 feat : 행성 개수에 따른 ui 작업
    - 행성 개수가 0일때 생성 컴포넌트 활성화
    - 행성 개수가 0일 때 스크롤 버튼 숨기기

--- src/components/PlanetList.vue
+++ src/components/PlanetList.vue
@@ -13,26 +13,30 @@
           :planet="planet"
         ></PlanetItem>
         <!-- todo 행성 생성 컴포넌트 추가 -->
-        <AddPlanetItem></AddPlanetItem>
+        <AddPlanetItem
+          :class="`${planets.length == 0 ? 'active' : ''}`"
+        ></AddPlanetItem>
       </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+      <template v-if="planets.length > 0">
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </template>
     </div>
   </div>
 </template>
