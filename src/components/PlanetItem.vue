48f6d651a3d9fb764886f51a647792e2397981f8
0andme <yu00mi97@gmail.com>
2023-02-20 13:51:25 +0900

    #2 feat : 프로세스 바 추가

--- src/components/PlanetItem.vue
+++ src/components/PlanetItem.vue
@@ -1,14 +1,42 @@
 <template>
   <div :class="`planet ${index === 0 ? 'active' : ''} carousel-item`">
     <div class="circle-wrap">
-      <img :src="require('@/assets/space-type-1.png')" alt="행성 생성하기" />
+      <ve-progress
+        id="#point-state-bar"
+        color="#E3F4FD"
+        empty-color="#394F7D"
+        :thickness="border"
+        :empty-thickness="`${border}`"
+        :progress="70"
+        :size="stateBarWidth"
+      >
+        <img
+          class="space-img"
+          :src="require('@/assets/space-type-1.png')"
+          alt="행성 이미지"
+        />
+      </ve-progress>
     </div>
   </div>
 </template>
 
 <script>
+import {veProgress} from "vue-ellipse-progress";
+
 export default {
   props: ["planet", "index"],
+  components: {veProgress},
+  data() {
+    return {
+      stateBarWidth: 0,
+      border: 0,
+    };
+  },
+  watch() {},
+  created() {
+    this.stateBarWidth = ($("#app").innerWidth() * 250) / 320;
+    this.border = parseInt($("html").css("fontSize")) * 0.8;
+  },
 };
 </script>
 
@@ -23,9 +51,39 @@ export default {
   right: 0;
 }
 
+.circle-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
 .circle-wrap img {
   display: block;
   margin: 0 auto;
-  width: calc(250 / 320 * 100%);
+  width: 20.5rem;
+}
+#point-state-bar {
+  width: 25rem;
+  height: 25rem;
+}
+.ep-content .ep-legend--container {
+  width: 100% !important;
+  height: 100% !important;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.ep-content .ep-legend--value {
+  width: 100% !important;
+  height: 100% !important;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#bg {
+  width: 100%;
+  background-color: red;
+  height: 100%;
 }
 </style>
