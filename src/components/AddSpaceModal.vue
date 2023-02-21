01b1baeb4cc4cd8b90a68df797a2f5c5eb79278a
0andme <yu00mi97@gmail.com>
2023-02-21 13:02:57 +0900

    #8 feat : 행성 생성 로직 추가

--- src/components/AddSpaceModal.vue
+++ src/components/AddSpaceModal.vue
@@ -1,14 +1,25 @@
 <template>
-  <LongModal :modalName="'add'">
-    <div class="msg">생성하고 싶은 행성을 선택하세요 !</div>
-    <div class="space-imgs">
-      <template v-for="spaceType in spaceTypeList" :key="spaceType.planet_id">
-        <div class="item">
-          <img :src="require(`@/assets/${spaceType.image}`)" alt="" />
-        </div>
-      </template>
-    </div>
-    <MakeSpaceBtn class="btn" :btnMsg="'생성하기'"></MakeSpaceBtn>
+  <LongModal :modalName="'add'" :title="step == 1 ? '행성 선택' : '혜택 선택'">
+    <template v-if="step === 1">
+      <div class="msg">생성하고 싶은 행성을 선택하세요 !</div>
+      <div class="space-imgs">
+        <template v-for="spaceType in spaceTypeList" :key="spaceType.planet_id">
+          <div
+            :class="`item ${
+              selected.planet_id === spaceType.planet_id ? 'select' : ''
+            }`"
+            @click="selectedSpace(spaceType)"
+          >
+            <img :src="require(`@/assets/${spaceType.image}`)" alt="" />
+          </div>
+        </template>
+      </div>
+      <MakeSpaceBtn
+        @click="nextStep"
+        class="btn"
+        :btnMsg="'선택하기'"
+      ></MakeSpaceBtn>
+    </template>
   </LongModal>
 </template>
 
@@ -18,13 +29,29 @@ import MakeSpaceBtn from "@/components/button/MakeSpaceBtn.vue";
 export default {
   data() {
     return {
-      spaceTypeList: [],
+      selected: {},
+      step: 1,
     };
   },
   components: {LongModal, MakeSpaceBtn},
   computed: {
     spaceTypeList() {
       return this.$store.state.planetTypes;
+    },
+  },
+  mounted() {
+    this.selected = this.spaceTypeList[0];
+  },
+  methods: {
+    selectedSpace(spaceType) {
+      if (this.selected === spaceType) {
+        this.selected = {};
+      } else {
+        this.selected = spaceType;
+      }
+    },
+    nextStep() {
+      this.step = 2;
     },
   },
 };
@@ -47,10 +74,12 @@ export default {
   margin-bottom: 3.8rem;
 }
 .space-imgs .item {
+  padding: 0.2rem;
 }
 .space-imgs .item.select {
   border: 0.2rem solid var(--point);
   border-radius: 1rem;
+  padding: 0;
 }
 .space-imgs .item img {
   width: 5.5rem;
