5857a45de1ae06f3fae11712fdaa90c9a4c27d54
kji-dec <dec22sag@gmail.com>
2023-02-21 11:03:58 +0900

    #7 feat: 미션 목록 컴포넌트 추가
    미션 카테고리 아래에 미션 컴포넌트 추가

--- src/views/PointMissionView.vue
+++ src/views/PointMissionView.vue
@@ -1,18 +1,22 @@
 <template>
     <point-title-bar></point-title-bar>
     <mission-category></mission-category>
+    <point-mission></point-mission>
 </template>
 
 <script>
 import PointTitleBar from '@/components/PointTitleBar.vue';
 import MissionCategory from '@/components/MissionCategory.vue';
+import PointMission from '@/components/PointMission.vue';
 export default {
     components: {
         PointTitleBar,
         MissionCategory,
+        PointMission,
     }
 };
 </script>
 
 <style>
+
 </style>
