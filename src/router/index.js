3d167902ac363f18ecd86dee5d102b172cc298b5
0andme <yu00mi97@gmail.com>
2023-02-21 15:38:34 +0900

    #10 test : api 연동 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,17 @@ import StartView from "@/views/StartView.vue";
 import GameView from "@/views/GameView.vue";
 import PointMissionView from "@/views/PointMissionView.vue";
 import ShopView from "@/views/ShopView.vue";
+import LoginView from "@/views/LoginView.vue";
 const routes = [
   {
     path: "/",
     name: "home",
     component: Home,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
     path: "/start",
---
+++ src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="login">로그인</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "username",
+      password: "password",
+    };
+  },
+  methods: {
+    login() {
+      const userData = {
+        username: "username",
+        password: "password",
+      };
+      fetch("/token", {
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  padding: 60px;
+  color: var(--text-white);
+}
+</style>
