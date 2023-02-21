23350ca0e13417803ae41233b4d08dae5902a115
kji-dec <dec22sag@gmail.com>
2023-02-21 10:03:20 +0900

    #7 feat: 미션 카테고리 컴포넌트 추가
    미션 카테고리 컴포넌트화, 페이지에 추가

---
+++ src/components/MissionCategory.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <strong class="category">웰컴 미션</strong>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        category(category) {
+            return category === "welcome" ? "웰컴 미션" : "데일리 미션"
+        }
+    }
+}
+</script>
+
+<style>
+.category {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 2.2rem;
+    color: #FFFFFF;
+    margin: 2.2rem;
+}
+</style>
--- src/views/PointMissionView.vue
+++ src/views/PointMissionView.vue
@@ -1,12 +1,15 @@
 <template>
     <point-title-bar></point-title-bar>
+    <mission-category></mission-category>
 </template>
 
 <script>
 import PointTitleBar from '@/components/PointTitleBar.vue';
+import MissionCategory from '@/components/MissionCategory.vue';
 export default {
     components: {
         PointTitleBar,
+        MissionCategory,
     }
 };
 </script>
