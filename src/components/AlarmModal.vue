ac378b0309b6b7252a108d5eb96b145827f8b977
kji-dec <dec22sag@gmail.com>
2023-02-21 16:33:20 +0900

    #7 feat: 미션 수행 완료 모달

---
+++ src/components/AlarmModal.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="modal fade" id="popUpModalTmp" tabindex="-1" aria-labelledby="popUpModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="modal-top">
+                        <img class="point" :src="require('@/assets/point.svg')" alt="포인트" />
+                        <div class="title">미션 포인트 획득</div>
+                    </div>
+                    <div class="content">마이 데이터 접속하기</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+import ConfrimBtn from "@/components/button/ConfrimBtn.vue";
+export default {
+    props: ["selectedSpace", "selectedReward"],
+    components: { ConfrimBtn },
+    methods: {
+        makePlanet() {
+            $("#popUpModal").modal("hide");
+            $("#longModalDepth-add").modal("hide");
+            // todo - 행성 생성 api호출
+        },
+    },
+
+};
+</script>
+  
+<style scoped>
+.modal-dialog {
+    width: 100%;
+    margin: 3.7rem 0 0;
+    display: flex;
+    align-items: top;
+    justify-content: center;
+}
+
+.modal-content {
+    width: 100%;
+    text-align: center;
+
+    border-radius: 20px;
+    background-color: var(--backGround);
+    box-sizing: content-box;
+}
+
+.modal-body {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 0;
+    margin: 2rem;
+}
+
+.modal-top {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0;
+    gap: 0.5rem;
+}
+
+.modal-body .title {
+    font-weight: 700;
+    font-size: 1.3rem;
+    line-height: 1.9rem;
+    text-align: center;
+    color: #CBCED8;
+}
+
+.modal-body .content {
+    font-weight: 700;
+    font-size: 1.6rem;
+    color: var(--text-white);
+    padding-left: 1.5rem;
+}
+</style>
+  
